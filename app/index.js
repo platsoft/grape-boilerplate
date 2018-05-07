c3449fd20446e85a3bc0b89b9e6885ed942fdbd6
Hans Lombard <hans@platsoft.net>
2018-05-07 14:39:41 +0200

    Initial commit

---
+++ app/index.js
@@ -0,0 +1,15 @@
+
+var Grape = require('ps-grape');
+
+var app = new Grape.grape(
+	"../config/defaults.json", 
+	"../config/local.json"); 
+
+// Register custom workers here
+
+app.start();
+
+app.on('worker-httplistener', function(app) {
+	// new express app created
+});
+
